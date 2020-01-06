2cd509c4c17f2dd1a5570bc1c959c009e8993369
Brian" Tatsuya Moriguchi <brian@beckos.com>
2020-01-06 14:53:23 -0800

    Initial Commit

---
+++ RESTfulAPI_SAMPLE3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RESTfulAPI_SAMPLE3
+//
+//  Created by Brian Moriguchi on 1/6/20.
+//  Copyright © 2020 Brian Moriguchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
